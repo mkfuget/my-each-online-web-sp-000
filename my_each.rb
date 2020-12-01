acd6198f74266397d5020326a1505a0063c1ee7c
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 09:11:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,6 +2,7 @@ def my_each(array)
   i = 0
   while i< array.length
     yield array[i]
+    i+=1
   end
   
   return array
@@ -11,4 +12,5 @@ collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
 end
+
 
